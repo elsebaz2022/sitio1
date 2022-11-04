0fb76bb5e6bebefeb23870b1250bbd0e389d1f8a
sebastianalexander-2022 <scpinto1234@gmail.com>
2022-11-03 20:54:03 -0500

    V02.1 Estoy implementado funciones para registrar en BD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import AppForm from "./componente/AppForm";
 
 function App() {
+  ///////////// READ //////////////
+  const [idActual, setIdActual] =useState('');                ////// PARA CREAR Y UPDATE ///////
+  const [docsBD, setDocsBD] = useState([]);                   ////// PARA LECTURA DE BD //////
+
+  //////////////// LECTURA A BD ////////////////////////
+  const fnRead =() => {
+    console.log("Lectura a BD");
+  }
+
+
+
+  const fnDelete =() => {
+    console.log("Eliminar un registro");
+  }
+
+
   return (
     <div style={{background:"greenyellow", width:"350px"}}>
-    <AppForm  />
+    <AppForm {...{idActual,setIdActual,fnRead}} />
     </div>
   );
 }
--- src/componente/AppForm.js
+++ src/componente/AppForm.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
-const AppForm = () => {
+const AppForm = (props) => {
+
+  const camposRegistro ={nombre:'',edad:'',genero:''};
+  const [objeto, setObjeto] = useState(camposRegistro);
+
+  const controlarEstadoCambio = (e) => {
+     
+  };
+
+  const controlSubmit = (e) => {
+     
+  };
+  
   return (
     <div style={{background:"orange"}}>
         Formulario AppForm.js
